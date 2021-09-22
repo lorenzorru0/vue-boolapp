076bcfbbfb2d50b6ebdec6145e0f280a847a33cc
lorenzorru0 <lorenzoorru999@gmail.com>
2021-09-22 12:27:11 +0200

    Add part of milestone 5

--- js/main.js
+++ js/main.js
@@ -10,17 +10,20 @@ const app = new Vue ({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        status: 'sent', 
+                        deleteMessage: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di dargli da mangiare',
-                        status: 'sent'
+                        status: 'sent',
+                        deleteMessage: false
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
-                        status: 'received'
+                        status: 'received',
+                        deleteMessage: false
                     }
                 ],
             },
@@ -31,17 +34,20 @@ const app = new Vue ({
                 messages: [{
                     date: '20/03/2020 16:30:00',
                     message: 'Ciao come stai?',
-                    status: 'sent'
+                    status: 'sent',
+                    deleteMessage: false
                 },
                 {
                     date: '20/03/2020 16:30:55',
                     message: 'Bene grazie! Stasera ci vediamo?',
-                    status: 'received'
+                    status: 'received',
+                    deleteMessage: false
                 },
                 {
                     date: '20/03/2020 16:35:00',
                     message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                    status: 'received'
+                    status: 'received',
+                    deleteMessage: false
                 }
                 ],
             },
@@ -52,17 +58,20 @@ const app = new Vue ({
                 messages: [{
                     date: '28/03/2020 10:10:40',
                     message: 'La Marianna va in campagna',
-                    status: 'received'
+                    status: 'received',
+                    deleteMessage: false
                 },
                 {
                     date: '28/03/2020 10:20:10',
                     message: 'Sicuro di non aver sbagliato chat?',
-                    status: 'sent'
+                    status: 'sent',
+                    deleteMessage: false
                 },
                 {
                     date: '28/03/2020 16:15:22',
                     message: 'Ah scusa!',
-                    status: 'received'
+                    status: 'received',
+                    deleteMessage: false
                 }
                 ],
             },
@@ -73,12 +82,14 @@ const app = new Vue ({
                 messages: [{
                     date: '10/01/2020 15:30:55',
                     message: 'Lo sai che ha aperto una nuova pizzeria?',
-                    status: 'sent'
+                    status: 'sent',
+                    deleteMessage: false
                 },
                 {
                     date: '10/01/2020 15:50:00',
                     message: 'Si, ma preferirei andare al cinema',
-                    status: 'received'
+                    status: 'received',
+                    deleteMessage: false
                 }
                 ],
             },
@@ -86,12 +97,117 @@ const app = new Vue ({
         currentContact: 0,
         newMessage: "",
         searchString: "",
-        testContacts: []
+        testContacts: [],
+        randomMessages: [
+            "Sì",
+            "È così.",
+            "Sarebbe bello.",
+            "E come?",
+            "Giustamente.",
+            "Sta bene.",
+            "Si può anche convenire.",
+            "Da quanto è stato detto almeno, pare così.",
+            "Di' pure come.",
+            "Sia pure così.",
+            "Come no?",
+            "Molto bene.",
+            "Certamente.",
+            "Tutto questo è stato assolutamente chiarito a sufficienza.",
+            "Così almeno sembra.",
+            "Bene.",
+            "Questo non lo sapevo.",
+            "È verissimo quel che dici.",
+            "Ma è chiaro!",
+            "Perfetto.",
+            "Infatti è così.",
+            "Come dici?",
+            "Ma per forza!",
+            "Ma certamente!",
+            "E perché non dovrei?",
+            "Si disse proprio così.",
+            "Come potrebbe essere diversamente?",
+            "Senza meno.",
+            "Eh già!",
+            "Dillo pure!",
+            "Si trattava proprio di questo.",
+            "Non ho capito.",
+            "Pare.",
+            "Bisogna ammetterlo senza riserva.",
+            "Sta proprio così.",
+            "È vero.",
+            "È assolutamente così.",
+            "Questo almeno è verosimile.",
+            "Dici bene.",
+            "Esattamente.",
+            "C'è proprio modo che questo avvenga soprattutto per questa ragione.",
+            "Perché no?",
+            "Difficilmente, sicuro!",
+            "Non potrebbe affatto.",
+            "Sicuro!",
+            "Veramente no.",
+            "E come non ammetterlo?",
+            "Tu dici bene: bisogna fare così.",
+            "Va chiamata così.",
+            "Ebbene?",
+            "Giusto.",
+            "Come e a quale scopo dici questo?",
+            "E perché?",
+            "È impossibile.",
+            "Assolutamente no.",
+            "È assolutamente necessario.",
+            "In che senso dici?",
+            "È molto chiaro.",
+            "Sì, nel modo più assoluto.",
+            "Ti seguo.",
+            "Pare certamente così.",
+            "Lo vedo anche troppo bene.",
+            "E che cos'altro potremmo dire?",
+            "Anche questo.",
+            "Mi pare che questo punto si debba dibattere nei nostri ragionamenti.",
+            "È verissimo.",
+            "Lo hai detto.",
+            "Per quel che mi riguarda non mi sembra affatto che tu sbagli.",
+            "Di' pure chiaro quel che vuoi dire.",
+            "Lo vedo.",
+            "Forse sì.",
+            "Eh, sì!",
+            "Tu dici il vero.",
+            "È molto giusto quello che dici.",
+            "È necessario.",
+            "Sembra senza dubbio che la questione stia così.",
+            "Sarà così.",
+            "Tutto questo a me pare che sia assurdo!",
+            "Certamente sì.",
+            "Verissimo.",
+            "Senza dubbio.",
+            "Assolutamente.",
+            "È proprio così.",
+            "È come dici.",
+            "Tu dici cosa autentica e vera.",
+            "Questo anch'io mi sento capace di affermarlo.",
+            "Sì, certamente.",
+            "Forse.",
+            "È esatto.",
+            "Ma questo è impossibile.",
+            "Pressappoco.",
+            "Ma certo.",
+            "Senza remora alcuna, assolutamente.",
+            "Nel modo più evidente.",
+            "Su per giù è così.",
+            "Dici il giusto.",
+            "È giustissimo.",
+            "A questo punto non ci sono arrivato.",
+            "È verosimile.",
+            "Tutto il contrario."
+        ]
     },
     mounted() {
         this.copyContacts()
     },
     methods: {
+        randomNumber: function(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        },
         copyContacts: function() {
             this.contacts.forEach((contact) => {
                 this.testContacts.push(contact);
@@ -107,20 +223,25 @@ const app = new Vue ({
             let dateTime = date + ' ' + time;
             return dateTime;
         },
+        getRandomMessage: function() {
+            return this.randomMessages[this.randomNumber(0, this.randomMessages.length - 1)]
+        },
         addMessage: function() {
             let currentDateTime = this.getDate();
             this.contacts[this.currentContact].messages.push({
                 date: currentDateTime,
                 message: this.newMessage,
-                status: 'sent'
+                status: 'sent',
+                deleteMessage: false
             });
             this.newMessage = "";
             setTimeout(() => { 
                 let currentDateTime = this.getDate();
                 this.contacts[this.currentContact].messages.push({
                     date: currentDateTime,
-                    message: 'Ciao!',
-                    status: 'received'
+                    message: this.getRandomMessage(),
+                    status: 'received',
+                    deleteMessage: false
                 });
             }, 1000);
         },
@@ -137,6 +258,9 @@ const app = new Vue ({
 
             this.testContacts = testArray;
 
+        },
+        toggleDeleteMessage: function(messageIndex) {
+            this.contacts[this.currentContact].messages[messageIndex].deleteMessage = !this.contacts[this.currentContact].messages[messageIndex].deleteMessage;
         }
     }
 });
